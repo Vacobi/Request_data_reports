c1ee63a1ec292fd5caf44b1022bce686e85823b7
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-15 15:09:54 +0300

    feat: added 'Request' entity

---
+++ src/main/java/axi/practice/data_generation_reports/entity/Request.java
@@ -0,0 +1,66 @@
+package axi.practice.data_generation_reports.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@Table(name="requests")
+public class Request {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", nullable = false)
+    private Long id;
+
+    @Column(name="url", nullable = false)
+    private String url;
+
+    @Column(name="path")
+    private String path;
+
+    @Column(name="body")
+    private String body;
+
+    @Column(name="timestamp")
+    private LocalDateTime timestamp;
+
+    @OneToMany(mappedBy = "request", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @OrderColumn(name = "order_id")
+    @Builder.Default
+    private List<Header> headers = new LinkedList<>();
+
+    @OneToMany(mappedBy = "request", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @OrderColumn(name = "order_id")
+    @Builder.Default
+    private List<QueryParam> queryParams = new LinkedList<>();
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "id=" + id +
+                ", url='" + url + '\'' +
+                ", path='" + path + '\'' +
+                ", body='" + (body != null ? body.length() + " chars" : "null") + '\'' +
+                ", timestamp=" + timestamp +
+                ", headers=" + (headers != null ?
+                headers.stream()
+                        .map(h -> "Header{id=" + h.getId() + ", name='" + h.getName() + "', value='" + h.getValue() + "'}")
+                        .toList()
+                : "null") +
+                ", queryParams=" + (queryParams != null ?
+                queryParams.stream()
+                        .map(p -> "QueryParam{id=" + p.getId() + ", name='" + p.getName() + "', value='" + p.getValue() + "'}")
+                        .toList()
+                : "null") +
+                '}';
+    }
+}

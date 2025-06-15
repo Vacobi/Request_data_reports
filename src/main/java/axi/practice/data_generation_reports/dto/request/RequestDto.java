7139ee89c878d38b74334165310e81fbf60c1d20
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-15 15:23:30 +0300

    feat: added dto for 'Request' entity

---
+++ src/main/java/axi/practice/data_generation_reports/dto/request/RequestDto.java
@@ -0,0 +1,28 @@
+package axi.practice.data_generation_reports.dto.request;
+
+import axi.practice.data_generation_reports.dto.header.HeaderDto;
+import axi.practice.data_generation_reports.dto.queryparam.QueryParamDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class RequestDto {
+    private Long id;
+    private String url;
+    private String path;
+    private String body;
+
+    @Builder.Default
+    private List<HeaderDto> headers = new LinkedList<>();
+
+    @Builder.Default
+    private List<QueryParamDto> variableParams = new LinkedList<>();
+}

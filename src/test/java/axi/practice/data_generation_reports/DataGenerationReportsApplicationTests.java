1d61d52a5c947e0552b634f980964bb9da309301
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-15 14:57:41 +0300

    feat: using TestContainersConfig in Application test class

--- src/test/java/axi/practice/data_generation_reports/DataGenerationReportsApplicationTests.java
+++ src/test/java/axi/practice/data_generation_reports/DataGenerationReportsApplicationTests.java
@@ -1,9 +1,12 @@
 package axi.practice.data_generation_reports;
 
+import axi.practice.data_generation_reports.config.TestContainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(initializers = TestContainersConfig.class)
 class DataGenerationReportsApplicationTests {
 
 	@Test

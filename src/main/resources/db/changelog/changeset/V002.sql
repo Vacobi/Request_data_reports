054ca8861a926b48fe40ace0a827fbb0fc117d03
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-17 22:51:02 +0300

    fix: fixed VIEW script

--- src/main/resources/db/changelog/changeset/V002.sql
+++ src/main/resources/db/changelog/changeset/V002.sql
@@ -1,26 +1,26 @@
 CREATE OR REPLACE VIEW group_request_stats AS
 WITH filter AS (
-    SELECT * FROM request_filters
-    WHERE id = (SELECT MAX(id) FROM request_filters)
+    SELECT * FROM filters_request
+    WHERE id = (SELECT MAX(id) FROM filters_request)
 ),
 filtered_requests AS (
     SELECT r.*
     FROM requests r
+    JOIN filter f ON TRUE
     WHERE (f.from_date IS NULL OR r.created_at >= f.from_date)
-    AND (f.to_date IS NULL OR r.created_at >= f.to_date)
-    AND (f.host IS NULL OR r.host = f.host)
+    AND (f.to_date IS NULL OR r.created_at <= f.to_date)
+    AND (f.host IS NULL OR r.url = f.host)
     AND (f.path IS NULL OR r.path = f.path)
 ),
 request_stats AS (
     SELECT
-        r.id,
-        r.url,
-        r.host,
-        r.path,
-        (SELECT COUNT(*) FROM headers h
-         WHERE h.request_id = r.id) AS header_count,
-        (SELECT COUNT(*) FROM query_params qp
-         WHERE qp.request_id = r.id) AS param_count
+    r.id,
+    r.url AS host,
+    r.path,
+    (SELECT COUNT(*) FROM headers h
+    WHERE h.request_id = r.id) AS header_count,
+    (SELECT COUNT(*) FROM query_params qp
+    WHERE qp.request_id = r.id) AS param_count
     FROM filtered_requests r
 )
 SELECT
@@ -30,8 +30,8 @@ SELECT
     AVG(rs.header_count) AS avg_headers,
     AVG(rs.param_count) AS avg_params
 FROM request_stats rs
-CROSS JOIN filter f
+JOIN filter f ON TRUE
 GROUP BY rs.host, rs.path
 HAVING
-    (f.avg_headers IS NULL OR AVG(rs.header_count) = f.avg_headers) AND
-    (f.avg_query_params IS NULL OR AVG(rs.param_count) = f.avg_query_params);
+    (MAX(f.avg_headers) IS NULL OR ROUND(AVG(rs.header_count), 2) = ROUND(MAX(f.avg_headers), 2)) AND
+    (MAX(f.avg_query_params) IS NULL OR ROUND(AVG(rs.param_count), 2) = ROUND(MAX(f.avg_query_params), 2));

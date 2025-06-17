acbcf6e9f154a6cc36cf923da5c7e5d370e63add
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-17 22:47:37 +0300

    refactor: removed duplicate of 'Table' annotation

--- src/main/java/axi/practice/data_generation_reports/entity/GroupRequestStat.java
+++ src/main/java/axi/practice/data_generation_reports/entity/GroupRequestStat.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
+import org.hibernate.annotations.Immutable;
 
 @Entity
 @NoArgsConstructor
@@ -13,7 +14,6 @@ import lombok.*;
 @Getter
 @Setter
 @Builder(toBuilder = true)
-@Table(name="request_filters")
 @Table(name="group_request_stats")
 public class GroupRequestStat {
 

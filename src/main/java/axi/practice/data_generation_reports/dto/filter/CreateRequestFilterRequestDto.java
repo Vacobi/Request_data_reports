b724c7e7f9249f4a1f60b91f68dec47fcdb7c14b
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-17 12:18:07 +0300

    feat: added CreateRequestFilterRequestDto

---
+++ src/main/java/axi/practice/data_generation_reports/dto/filter/CreateRequestFilterRequestDto.java
@@ -0,0 +1,21 @@
+package axi.practice.data_generation_reports.dto.filter;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class CreateRequestFilterRequestDto {
+    private LocalDateTime fromDate;
+    private LocalDateTime toDate;
+    private String host;
+    private String path;
+    private Integer avgHeaders;
+    private Integer avgQueryParams;
+}

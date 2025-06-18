149e14a5dadfc9d144166fa45ad4380010f5d05e
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-18 13:07:20 +0300

    delete: removed unused method in dao

--- src/main/java/axi/practice/data_generation_reports/dao/GroupRequestStatDao.java
+++ src/main/java/axi/practice/data_generation_reports/dao/GroupRequestStatDao.java
@@ -1,18 +1,12 @@
 package axi.practice.data_generation_reports.dao;
 
 import axi.practice.data_generation_reports.entity.GroupRequestStat;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface GroupRequestStatDao extends JpaRepository<GroupRequestStat, Long> {
+import java.util.UUID;
 
-    @Query("""
-            SELECT g
-            FROM GroupRequestStat g
-           """)
-    Page<GroupRequestStat> findAllFilteredGroupRequestStats(Pageable pageable);
+@Repository
+public interface GroupRequestStatDao extends JpaRepository<GroupRequestStat, UUID> {
+
 }

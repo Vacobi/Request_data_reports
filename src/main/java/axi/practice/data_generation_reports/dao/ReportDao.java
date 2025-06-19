797c494e528b40560a5344c1c725647870d3e8bb
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-20 00:32:35 +0300

    feat: added dao for 'Report'

---
+++ src/main/java/axi/practice/data_generation_reports/dao/ReportDao.java
@@ -0,0 +1,16 @@
+package axi.practice.data_generation_reports.dao;
+
+import axi.practice.data_generation_reports.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ReportDao extends JpaRepository<Report, Long> {
+
+    @Query("SELECT r FROM Report r LEFT JOIN FETCH r.rows WHERE r.id = :id")
+    Optional<Report> findByIdWithRows(@Param("id") Long id);
+}

55f45d76dbbf06ca4d5e31c79422003a139388bf
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-20 00:34:12 +0300

    feat: added dto for 'ReportRow'

---
+++ src/main/java/axi/practice/data_generation_reports/dto/report_row/ReportRowDto.java
@@ -0,0 +1,21 @@
+package axi.practice.data_generation_reports.dto.report_row;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class ReportRowDto {
+    private Long id;
+    private UUID rowUUID;
+    private String host;
+    private String path;
+    private double avgHeaders;
+    private double avgQueryParams;
+}

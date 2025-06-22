7475cfc35e99588f3822356642e22f0891cc5e40
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-23 00:45:45 +0300

    feat: added method for getting Page of ReportRows by Report id in 'ReportRowDao'

--- src/main/java/axi/practice/data_generation_reports/dao/ReportRowDao.java
+++ src/main/java/axi/practice/data_generation_reports/dao/ReportRowDao.java
@@ -1,10 +1,14 @@
 package axi.practice.data_generation_reports.dao;
 
+import axi.practice.data_generation_reports.entity.Report;
 import axi.practice.data_generation_reports.entity.ReportRow;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReportRowDao extends JpaRepository<ReportRow, Long> {
 
+    Page<ReportRow> findByReport(Report report, Pageable pageable);
 }

928a56f5d06fb93629fecc9b11ab827dbbc3fe58
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-23 09:37:56 +0300

    refactor: removed unused map methods

--- src/main/java/axi/practice/data_generation_reports/mapper/ReportMapper.java
+++ src/main/java/axi/practice/data_generation_reports/mapper/ReportMapper.java
@@ -1,10 +1,9 @@
 package axi.practice.data_generation_reports.mapper;
 
-import axi.practice.data_generation_reports.dto.report.ReportDto;
+import axi.practice.data_generation_reports.dto.report.ReportDataDto;
 import axi.practice.data_generation_reports.entity.Report;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(
@@ -15,8 +14,5 @@ import org.mapstruct.ReportingPolicy;
 )
 public interface ReportMapper {
 
-    @Mapping(source = "rows", target = "rows")
-    ReportDto toReportDto(Report persisted);
-
-    Report toReport(ReportDto request);
+    ReportDataDto toReportDataDto(Report report);
 }

2496c379db1f907cc2f22ba5c91dc7b5bbb89a6b
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-23 09:39:12 +0300

    feat: added dto for report data

---
+++ src/main/java/axi/practice/data_generation_reports/dto/report/ReportDataDto.java
@@ -0,0 +1,22 @@
+package axi.practice.data_generation_reports.dto.report;
+
+import axi.practice.data_generation_reports.dto.filter.RequestFilterDto;
+import axi.practice.data_generation_reports.entity.enums.ReportStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class ReportDataDto {
+    private Long id;
+    private RequestFilterDto filter;
+    private ReportStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime finishedAt;
+}

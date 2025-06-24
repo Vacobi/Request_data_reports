45b15c5c0fa7cfc2fed041d8685c43487f86157c
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-24 11:17:10 +0300

    refactor: removed unused import

--- src/main/java/axi/practice/data_generation_reports/service/ReportService.java
+++ src/main/java/axi/practice/data_generation_reports/service/ReportService.java
@@ -21,7 +21,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
